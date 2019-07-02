14e2702c6ad156c533bc1d5ee1f4734949f01fd1
denis <dedi.ceka99@gmail.com>
2019-07-02 16:00:45 +0200

    fix browserModule that prevents lazyModule

--- projects/select-autocomplete/src/lib/select-autocomplete.module.ts
+++ projects/select-autocomplete/src/lib/select-autocomplete.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { SelectAutocompleteComponent } from './select-autocomplete.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-
 @NgModule({
-  imports: [BrowserAnimationsModule, MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule,
-    MatIconModule, MatCheckboxModule, MatButtonModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   declarations: [SelectAutocompleteComponent],
   exports: [SelectAutocompleteComponent]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { SelectAutocompleteModule } from 'projects/select-autocomplete/src/public_api';
 
 import { AppComponent } from './app.component';
-import { SelectAutocompleteModule } from 'select-autocomplete';
 
 @NgModule({
   declarations: [
