9210733068e4906afe9e69a3f2b2fe844b4801fc
Maloth Naresh <idofmalothnaresh@gmail.com>
2019-07-21 19:21:27 +0530

    Open issue - Enter the invalid the chars, the Select All(Check box) box - automatically selected : Fixed

--- projects/select-autocomplete/src/lib/select-autocomplete.component.ts
+++ projects/select-autocomplete/src/lib/select-autocomplete.component.ts
@@ -6,95 +6,120 @@ import {
   Output,
   ViewChild,
   DoCheck
-} from '@angular/core';
-import { FormControl } from '@angular/forms';
+} from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'mat-select-autocomplete',
+  selector: "mat-select-autocomplete",
   template: `
-  <mat-form-field appearance="{{appearance}}">
-    <mat-select #selectElem [disabled]="disabled" [placeholder]="placeholder" [formControl]="formControl" [multiple]="multiple"
-    [(ngModel)]="selectedValue" (selectionChange)="onSelectionChange($event)">
-    <div class="box-search">
-        <mat-checkbox *ngIf="multiple" color="primary" class="box-select-all" [(ngModel)]="selectAllChecked"
-        (change)="toggleSelectAll($event)"></mat-checkbox>
-        <input #searchInput type="text" [ngClass]="{'pl-1': !multiple}" (input)="filterItem(searchInput.value)" 
-          [placeholder]="selectPlaceholder">
-        <div class="box-search-icon" (click)="filterItem(''); searchInput.value = ''">
-          <button mat-icon-button class="search-button">
-            <mat-icon class="mat-24" aria-label="Search icon">clear</mat-icon>
-          </button>
+    <mat-form-field appearance="{{ appearance }}">
+      <mat-select
+        #selectElem
+        [disabled]="disabled"
+        [placeholder]="placeholder"
+        [formControl]="formControl"
+        [multiple]="multiple"
+        [(ngModel)]="selectedValue"
+        (selectionChange)="onSelectionChange($event)"
+      >
+        <div class="box-search">
+          <mat-checkbox
+            *ngIf="multiple"
+            color="primary"
+            class="box-select-all"
+            [(ngModel)]="selectAllChecked"
+            (change)="toggleSelectAll($event)"
+          ></mat-checkbox>
+          <input
+            #searchInput
+            type="text"
+            [ngClass]="{ 'pl-1': !multiple }"
+            (input)="filterItem(searchInput.value)"
+            [placeholder]="selectPlaceholder"
+          />
+          <div
+            class="box-search-icon"
+            (click)="filterItem(''); searchInput.value = ''"
+          >
+            <button mat-icon-button class="search-button">
+              <mat-icon class="mat-24" aria-label="Search icon">clear</mat-icon>
+            </button>
+          </div>
         </div>
-      </div>
-      <mat-select-trigger>
-        {{onDisplayString()}}
-      </mat-select-trigger>
-        <mat-option *ngFor="let option of options" [disabled]="option.disabled" [value]="option[value]"
-          [style.display]="hideOption(option) ? 'none': 'flex'">{{option[display]}}
+        <mat-select-trigger>
+          {{ onDisplayString() }}
+        </mat-select-trigger>
+        <mat-option
+          *ngFor="let option of options"
+          [disabled]="option.disabled"
+          [value]="option[value]"
+          [style.display]="hideOption(option) ? 'none' : 'flex'"
+          >{{ option[display] }}
         </mat-option>
-    </mat-select>
-    <mat-hint style="color:red" *ngIf="showErrorMsg">{{errorMsg}}</mat-hint>
-  </mat-form-field>
+      </mat-select>
+      <mat-hint style="color:red" *ngIf="showErrorMsg">{{ errorMsg }}</mat-hint>
+    </mat-form-field>
   `,
   styles: [
     `
-    .box-search {
-      margin: 8px;
-      border-radius: 2px;
-      box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);
-      transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);
-      display: flex;
-    }
-    .box-search input {
-      flex: 1;
-      border: none;
-      outline: none;
-    }
-    .box-select-all {
-      width: 36px;
-      line-height: 33px;
-      color: #808080;
-      text-align: center;
-    }
-    .search-button {
-      width: 36px;
-      height: 36px;
-      line-height: 33px;
-      color: #808080;
-    }
-    .pl-1 {
-      padding-left: 1rem;
-    }`
+      .box-search {
+        margin: 8px;
+        border-radius: 2px;
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16),
+          0 0 0 1px rgba(0, 0, 0, 0.08);
+        transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);
+        display: flex;
+      }
+      .box-search input {
+        flex: 1;
+        border: none;
+        outline: none;
+      }
+      .box-select-all {
+        width: 36px;
+        line-height: 33px;
+        color: #808080;
+        text-align: center;
+      }
+      .search-button {
+        width: 36px;
+        height: 36px;
+        line-height: 33px;
+        color: #808080;
+      }
+      .pl-1 {
+        padding-left: 1rem;
+      }
+    `
   ]
 })
 export class SelectAutocompleteComponent implements OnChanges, DoCheck {
-
-  @Input() selectPlaceholder: string = 'search...';
+  @Input() selectPlaceholder: string = "search...";
   @Input() placeholder: string;
   @Input() options;
   @Input() disabled = false;
-  @Input() display = 'display';
-  @Input() value = 'value';
+  @Input() display = "display";
+  @Input() value = "value";
   @Input() formControl: FormControl = new FormControl();
-  @Input() errorMsg: string = 'Field is required';
+  @Input() errorMsg: string = "Field is required";
   @Input() showErrorMsg = false;
   @Input() selectedOptions;
   @Input() multiple = true;
 
   // New Options
   @Input() labelCount: number = 1;
-  @Input() appearance: 'standard' | 'fill' | 'outline' = 'standard';
+  @Input() appearance: "standard" | "fill" | "outline" = "standard";
 
   @Output()
   selectionChange: EventEmitter<any> = new EventEmitter();
 
-  @ViewChild('selectElem') selectElem;
+  @ViewChild("selectElem") selectElem;
 
   filteredOptions: Array<any> = [];
   selectedValue: Array<any> = [];
   selectAllChecked = false;
-  displayString = '';
-  constructor() { }
+  displayString = "";
+  constructor() {}
 
   ngOnChanges() {
     this.filteredOptions = this.options;
@@ -141,6 +166,9 @@ export class SelectAutocompleteComponent implements OnChanges, DoCheck {
         this.selectAllChecked = false;
       }
     });
+    if (!this.filteredOptions.length) {
+      this.selectAllChecked = false;
+    }
   }
 
   hideOption(option) {
@@ -157,7 +185,7 @@ export class SelectAutocompleteComponent implements OnChanges, DoCheck {
   }
 
   onDisplayString() {
-    this.displayString = '';
+    this.displayString = "";
     if (this.selectedValue && this.selectedValue.length) {
       let displayOption = [];
       if (this.multiple) {
@@ -169,11 +197,12 @@ export class SelectAutocompleteComponent implements OnChanges, DoCheck {
         }
         if (displayOption.length) {
           for (let i = 0; i < displayOption.length; i++) {
-            this.displayString += displayOption[i][this.display] + ',';
+            this.displayString += displayOption[i][this.display] + ",";
           }
           this.displayString = this.displayString.slice(0, -1);
           if (this.selectedValue.length > 1) {
-            this.displayString += ` (+${this.selectedValue.length - this.labelCount} others)`;
+            this.displayString += ` (+${this.selectedValue.length -
+              this.labelCount} others)`;
           }
         }
       } else {
@@ -203,5 +232,4 @@ export class SelectAutocompleteComponent implements OnChanges, DoCheck {
     this.selectedValue = val.value;
     this.selectionChange.emit(this.selectedValue);
   }
-
 }
