fa1a492364798d54b5314c63a2889aad7f995959
Raymond <rattanakchea@users.noreply.github.com>
2021-10-25 21:24:23 -0700

    fix MatButton MatCheckbox import

--- projects/select-autocomplete/src/lib/select-autocomplete.module.ts
+++ projects/select-autocomplete/src/lib/select-autocomplete.module.ts
@@ -4,9 +4,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+// import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { SelectAutocompleteComponent } from './select-autocomplete.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
   imports: [
     FormsModule,
